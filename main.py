a7de0c52bd98cb2e70d8f11520ed40e9116ca1a8
Debamitro Chakraborti <debamitro@gmail.com>
2022-02-01 16:32:41 -0500

    Ignore universal set of exons when drawing each tree

--- main.py
+++ main.py
@@ -9,18 +9,21 @@ from analyze_sequences import analyze_sequences
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Usage is {0} <gtf-file-name> <gene-name>".format(sys.argv[0]))
-        sys.exit (0)
+        sys.exit(0)
 
     transcripts = read_gtf(sys.argv[1], sys.argv[2])
     draw_transcripts(transcripts)
     forest = analyze_sequences(transcripts)
     i = 1
     for tree in forest["trees"]:
-        sequences = {
-            "exons" : forest["exons"],
-            "sequences" : tree
-        }
-        draw_exon_sequence_graph(sequences,
-                                 title="Tree {0}".format(i),
-                                 to_scale=False)
+        sequences = {"exons": [], "sequences": tree}
+        all_exons = set()
+        for sequence in tree:
+            for exon in sequence:
+                all_exons.add(exon)
+        for exon in all_exons:
+            sequences["exons"].append(exon)
+        sequences["exons"].sort()
+
+        draw_exon_sequence_graph(sequences, title="Tree {0}".format(i), to_scale=False)
         i += 1
