2cece4de017dfd3b62853b0537b9e5b60977cc62
Debamitro Chakraborti <debamitro@gmail.com>
2022-02-03 06:55:46 -0500

    Used draw_exon_sequence_forest to draw all trees at once

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import sys
 
 from read_gtf import read_gtf
-from draw_exons import draw_exons, draw_transcripts, draw_exon_sequence_graph
+from draw_exons import draw_transcripts, draw_exon_sequence_forest
 from analyze_sequences import analyze_sequences
 
 if __name__ == "__main__":
@@ -14,16 +14,4 @@ if __name__ == "__main__":
     transcripts = read_gtf(sys.argv[1], sys.argv[2])
     draw_transcripts(transcripts)
     forest = analyze_sequences(transcripts)
-    i = 1
-    for tree in forest["trees"]:
-        sequences = {"exons": [], "sequences": tree}
-        all_exons = set()
-        for sequence in tree:
-            for exon in sequence:
-                all_exons.add(exon)
-        for exon in all_exons:
-            sequences["exons"].append(exon)
-        sequences["exons"].sort()
-
-        draw_exon_sequence_graph(sequences, title="Tree {0}".format(i), to_scale=False)
-        i += 1
+    draw_exon_sequence_forest(forest)
